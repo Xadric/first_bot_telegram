217631d90074bf96d48d8a5b3e49a499ac362fe7
Xadric <artem.prime2008@gmail.com>
2022-07-09 14:22:55 +0300

    answer Hello-> Sticer(hi)+can send photo answer sendPhoto -> RandomPhoto(1,2,3,4)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,22 +4,36 @@ import org.telegram.telegrambots.bots.*;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.send.SendSticker;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.stickers.Sticker;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 class MySuperBot extends TelegramLongPollingBot{
     public void onUpdateReceived(Update update){
-        var massage = update.getMessage().getText();
+        var message = update.getMessage().getText();
         var chatId = update.getMessage().getChatId();
-        System.out.println(massage);
+        Random random = new Random();
+
         try {
-            if massage.equals()
-            sendMassage(chatId,"Hello");
-            sendPhoto(chatId);
+
+
+            if (message.equals("Hello")){
+                sendPhoto(chatId,"hello.png");
+                sendMassage(chatId, "I can send a photo (/sendPhoto) \n");
+            } else if (message.equals("/sendPhoto")) {
+                sendPhoto(chatId,random.nextInt(4)+".jpg");
+            } else{
+                sendMassage(chatId, "??????????");
+            }
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -34,15 +48,19 @@ class MySuperBot extends TelegramLongPollingBot{
         execute(massege);
     }
 
-    void sendPhoto(long chatId) throws Exception{
-        var name = new Random().nextInt(2);
-        var photo = getClass().getClassLoader().getResourceAsStream(name+".jpg");
+    void sendPhoto(long chatId , String name) throws Exception{
+        var photo = getClass().getClassLoader().getResourceAsStream(name);
 
         var message = new SendPhoto();
         message.setChatId(String.valueOf(chatId));
         message.setPhoto(new InputFile(photo, "photo"));
         execute(message);
     }
+
+
+
+
+
 
 
     public String getBotUsername(){
